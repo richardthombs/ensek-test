543ddbc89efed4ec02c029859a9700f7f187f7fb
Richard Thombs <richard@gearstone.uk>
2021-03-11 07:53:02 +0000

    Update EF Core migration

--- api/EnsekTest.WebApi/Migrations/20210311075246_v002.Designer.cs
+++ api/EnsekTest.WebApi/Migrations/20210311075246_v002.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using EnsekTest.Integrations.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EnsekTest.WebApi.Migrations
 {
     [DbContext(typeof(MeterReadingContext))]
-    partial class MeterReadingContextModelSnapshot : ModelSnapshot
+    [Migration("20210311075246_v002")]
+    partial class v002
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ api/EnsekTest.WebApi/Migrations/20210311075246_v002.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EnsekTest.WebApi.Migrations
+{
+    public partial class v002 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MeterReadings",
+                table: "MeterReadings");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MeterReadingId",
+                table: "MeterReadings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MeterReadings",
+                table: "MeterReadings",
+                column: "MeterReadingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MeterReadings_AccountId_MeterReadingDateTime",
+                table: "MeterReadings",
+                columns: new[] { "AccountId", "MeterReadingDateTime" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MeterReadings",
+                table: "MeterReadings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MeterReadings_AccountId_MeterReadingDateTime",
+                table: "MeterReadings");
+
+            migrationBuilder.DropColumn(
+                name: "MeterReadingId",
+                table: "MeterReadings");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MeterReadings",
+                table: "MeterReadings",
+                columns: new[] { "AccountId", "MeterReadingDateTime" });
+        }
+    }
+}
